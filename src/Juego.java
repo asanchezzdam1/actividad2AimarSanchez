cfe9fa8a5adb3adb32178b527802d31a3ffd1985
castellano <asanchezzdam1@gmail.com>
2022-05-16 17:31:27 +0200

    palabra piedra añadida a una constante

--- src/Juego.java
+++ src/Juego.java
@@ -7,6 +7,9 @@ package ende;
 
 public class Juego 
 {
+
+    public static final String PIEDRA = "piedra";
+
     public static void main(String args[])
     {
         Jugador p1=new Jugador();
@@ -17,7 +20,7 @@ public class Juego
         Integer EXITOS_jugador2=p2.éxitos;
         Integer EmPaTeS = 0;
         String opcion_JUGADOR1, opcion_JUGADOR2;
-
+        
         // Bucle de juego
         do
         {
@@ -27,12 +30,12 @@ public class Juego
             System.out.println("Jugador 1: " + opcion_JUGADOR1+"\t Jugador 1 - Partidas ganadas: " + EXITOS_jugador1);
             opcion_JUGADOR2 = p2.opcion_al_azar();
             System.out.println("Jugador 2: " + opcion_JUGADOR2+"\t Jugador 2 - Partidas ganadas: " + EXITOS_jugador2);
-
+            
             if((opcion_JUGADOR1.equals("piedra"))&&(opcion_JUGADOR2.equals("papel")))
             {
                 System.out.println("Jugador 2 GANA");
                 EXITOS_jugador2 = ++p2.éxitos;
-
+                
             }
             else if((opcion_JUGADOR1.equals("papel"))&&(opcion_JUGADOR2.equals("piedra")))
             {
@@ -44,7 +47,7 @@ public class Juego
             	EXITOS_jugador1 = ++p1.éxitos;
                 System.out.println("Jugador 1 GANA");
             }
-            else if((opcion_JUGADOR1.equals("tijeras"))&&(opcion_JUGADOR2.equals("piedra")))
+            else if((opcion_JUGADOR1.equals("tijeras"))&&(opcion_JUGADOR2.equals(PIEDRA)))
             {
             	EXITOS_jugador2 = ++p2.éxitos;
                 System.out.println("Jugador 2 GANA");
@@ -78,7 +81,9 @@ public class Juego
  *
  */
 class Jugador{
-   
+
+    public static final String PIEDRA = "piedra";
+
     /**
      * Escoge piedra, papel o tijera al azar
      */
@@ -88,7 +93,7 @@ class Jugador{
         Integer c = (int)(Math.random()*3);
         switch(c){
             case 0:
-            	opcion=("piedra");
+            	opcion= PIEDRA;
                 break;
             case 1:
             	opcion=("papel");
